82413820381ac63766681182d6103051a330027c
Eunhyeon <85001394+eunhyeon1@users.noreply.github.com>
2024-05-10 02:26:04 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	double a = 0, b = 0;
+	cin >> a >> b;
+	cout.precision(10);
+	cout << a / b << endl;
+	return 0;
+}
